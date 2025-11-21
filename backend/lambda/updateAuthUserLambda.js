7a6ceeb577a8246d0a1dfeccecebcc7073f8bad6
Jack99 <25029149+cyril-bonneau@users.noreply.github.com>
2025-11-21 13:37:48 +0100

    try to patch an issue before it  occure

--- backend/lambda/updateAuthUserLambda.js
+++ backend/lambda/updateAuthUserLambda.js
@@ -101,7 +101,7 @@ async function updateUserTransactional(params) {
             newEmail,
         });
         console.log("addEmailChangeOperationsResult", addEmailChangeOperationsResult)
-        transactItems.push(addEmailChangeOperationsResult)
+        transactItems.push({ addEmailChangeOperationsResult })
         console.log("transactItems hasEmailChange", transactItems)
     }
 
@@ -113,7 +113,7 @@ async function updateUserTransactional(params) {
             lastName,
         });
         console.log("addUserProfileUpdateOperationResult", addUserProfileUpdateOperationResult)
-        transactItems.push(addUserProfileUpdateOperationResult)
+        transactItems.push({ addUserProfileUpdateOperationResult })
         console.log("transactItems hasProfileChange", transactItems)
     }
 
@@ -126,7 +126,7 @@ async function updateUserTransactional(params) {
                 transactItems
             })
             console.log("updatePasswordCore", updatePasswordCoreResult)
-            transactItems.push(updatePasswordCoreResult)
+            transactItems.push({ updatePasswordCoreResult })
             console.log("transactItems hasPasswordChange", transactItems)
         } catch (err) {
             if (err.code === "WRONG_PASSWORD") {
@@ -147,6 +147,8 @@ async function updateUserTransactional(params) {
             hasPasswordChange,
             opsCount: transactItems.length,
         });
+
+        console.log("final transactItems", transactItems)
 
         await sendToDb(transactItems)
 
