78d0e49754ddcdb76bfa472286b4e3795496e1e6
Jack99 <25029149+cyril-bonneau@users.noreply.github.com>
2025-12-04 22:56:29 +0100

    je suis un gland

--- backend/dal/checkPasswordByEmail.js
+++ backend/dal/checkPasswordByEmail.js
@@ -1,4 +1,5 @@
 import {
+    GetCommand,
     QueryCommand
 } from "@aws-sdk/lib-dynamodb";
 import { ddb } from "./requestToDb.js";
@@ -6,6 +7,7 @@ import { ddb } from "./requestToDb.js";
 import { verifyPassword } from "../auth/auth.js";
 
 const AUTH_TABLE = process.env.AUTH_TABLE;
+const USER_TABLE = process.env.USER_TABLE;
 
 export async function checkPasswordByEmail({ password, email }) {
     const auth = await getAuthByEmail(email)
@@ -32,16 +34,36 @@ async function getAuthByEmail(email) {
         const res = await ddb.send(
             new QueryCommand({
                 TableName: AUTH_TABLE,
-                IndexName: "GSI1v6",
+                IndexName: "GSI1v5",
                 KeyConditionExpression: "GSI1PK = :pk",
                 ExpressionAttributeValues: {
                     ":pk": `EMAIL#${email}`,
                 },
-                ProjectionExpression: "passwordHash, userId, privilege",
+                ProjectionExpression: "passwordHash, userId",
             })
         )
         console.log("getAuthByEmail result:", res.Items[0]);
+        res.Items[0].privilege = await getPrivilegeByUserId(res.Items[0].userId)
         return res.Items[0]
+    } catch (err) {
+        return err
+    }
+}
+
+async function getPrivilegeByUserId(userId) {
+    try {
+        const res = await ddb.send(
+            new GetCommand({
+                TableName: USER_TABLE,
+                Key: {
+                    "PK": `USER#${userId}`,
+                    "SK": `PROFILE#${userId}`
+                },
+                ProjectionExpression: "privilege",
+            })
+        )
+        console.log("getPrivilegeByUserId result:", res.Items[0]);
+        return res.Items[0]?.privilege
     } catch (err) {
         return err
     }
