99b9449fb34317d93ad5fc869639b66acbf45407
Jack99 <25029149+cyril-bonneau@users.noreply.github.com>
2025-12-05 12:50:49 +0100

    test

--- backend/tests/integration/authFlow.int.test.mjs
+++ backend/tests/integration/authFlow.int.test.mjs
@@ -44,6 +44,8 @@ describe("Parcours complet Auth (intégration)", () => {
         expect(response.body.ok).toBe(true);
         expect(response.body.userId).toBeDefined();
         expect(response.body.message).toBe("user successfully created");
+        expect(response.body.accessToken).toBeDefined();
+        console.log("RegisterUser Lambda response body:", response.body.accessToken);
 
         userId = response.body.userId;
         console.log("Created userId:", userId);
