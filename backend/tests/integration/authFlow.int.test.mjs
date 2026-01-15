b481158f3a46cce5753370130f276226f3d05277
Jack99 <25029149+cyril-bonneau@users.noreply.github.com>
2026-01-15 14:56:11 +0100

    updateCommand turned to function

--- backend/tests/integration/authFlow.int.test.mjs
+++ backend/tests/integration/authFlow.int.test.mjs
@@ -116,16 +116,7 @@ describe("Parcours complet Auth (intégration)", () => {
             Cookie: refreshToken,
         }
 
-        const payload = await client.send(
-            new InvokeCommand({
-                FunctionName: `site-asso-api-${process.env.STAGE}-updateAuthUser`,
-                Payload: Buffer.from(JSON.stringify({
-                    queryStringParameters: { id: userId },
-                    headers: headers,
-                    body: JSON.stringify(body),
-                })),
-            })
-        );
+        const payload = await updateCommand(userId, headers, body);
 
         let response = decode(payload.Payload);
 
@@ -149,16 +140,7 @@ describe("Parcours complet Auth (intégration)", () => {
             Cookie: refreshToken,
         }
 
-        const payload2 = await client.send(
-            new InvokeCommand({
-                FunctionName: `site-asso-api-${process.env.STAGE}-updateAuthUser`,
-                Payload: Buffer.from(JSON.stringify({
-                    queryStringParameters: { id: userId },
-                    headers: headers,
-                    body: JSON.stringify(body),
-                })),
-            })
-        );
+        const payload2 = await updateCommand(userId, headers, body);
 
         console.log('body.newEmail after update', body.newEmail);
         response = decode(payload2.Payload);
@@ -285,3 +267,16 @@ describe("Parcours complet Auth (intégration)", () => {
     });
 
 });
+
+async function updateCommand(userId, headers, body) {
+    await client.send(
+        new InvokeCommand({
+            FunctionName: `site-asso-api-${process.env.STAGE}-updateAuthUser`,
+            Payload: Buffer.from(JSON.stringify({
+                queryStringParameters: { id: userId },
+                headers: headers,
+                body: JSON.stringify(body),
+            })),
+        })
+    );
+}
