25001265490b92f0a42667ac95e2fb840d0f7e15
Jack99 <25029149+cyril-bonneau@users.noreply.github.com>
2026-07-03 12:16:57 +0200

    skip tests

--- backend/tests/integration/créationAssoFlow.int.test.mjs
+++ backend/tests/integration/créationAssoFlow.int.test.mjs
@@ -1,170 +1,170 @@
-// import { describe, it, beforeAll, expect } from "vitest";
-// import { getUserProfileByUserId } from "../testDbRequest/get.js";
-// import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
-// import { decode } from "../../helpers/toolbox.js";
+import { describe, it, beforeAll, expect } from "vitest";
+import { getUserProfileByUserId } from "../testDbRequest/get.js";
+import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
+import { decode } from "../../helpers/toolbox.js";
 
-// const client = new LambdaClient({ region: "eu-west-3" });
+const client = new LambdaClient({ region: "eu-west-3" });
 
-// // === Données de test ===
-// // email unique à chaque run pour éviter les collisions en dev
-// const timestamp = Date.now();
-// const TEST_EMAIL = `cyril.bonneau${timestamp}@outlook.com`;
-// const TEST_PASSWORD = "Ax9!qL7#vZ3@pT2";
-// const TEST_FIRSTNAME = "Jack";
-// const TEST_LASTNAME = "Larnaque";
+// === Données de test ===
+// email unique à chaque run pour éviter les collisions en dev
+const timestamp = Date.now();
+const TEST_EMAIL = `cyril.bonneau${timestamp}@outlook.com`;
+const TEST_PASSWORD = "Ax9!qL7#vZ3@pT2";
+const TEST_FIRSTNAME = "Jack";
+const TEST_LASTNAME = "Larnaque";
 
-// var userId;
-// var accessToken;
-// var refreshToken;
-// var assoId;
+var userId;
+var accessToken;
+var refreshToken;
+var assoId;
 
-// describe("Parcours complet création d'association (intégration)", () => {
-//     beforeAll(async () => {
+describe.skip("Parcours complet création d'association (intégration)", () => {
+    beforeAll(async () => {
 
-//         const body = {
-//             email: TEST_EMAIL,
-//             password: TEST_PASSWORD,
-//             firstName: TEST_FIRSTNAME,
-//             lastName: TEST_LASTNAME,
-//         }
+        const body = {
+            email: TEST_EMAIL,
+            password: TEST_PASSWORD,
+            firstName: TEST_FIRSTNAME,
+            lastName: TEST_LASTNAME,
+        }
 
-//         const payload = await client.send(
-//             new InvokeCommand({
-//                 FunctionName: `site-asso-api-${process.env.STAGE}-registerUser`,
-//                 Payload: Buffer.from(JSON.stringify({ body: JSON.stringify(body) })),
-//             })
-//         );
+        const payload = await client.send(
+            new InvokeCommand({
+                FunctionName: `site-asso-api-${process.env.STAGE}-registerUser`,
+                Payload: Buffer.from(JSON.stringify({ body: JSON.stringify(body) })),
+            })
+        );
 
-//         const response = decode(payload.Payload);
+        const response = decode(payload.Payload);
 
-//         console.log("RegisterUser Lambda response:", response);
+        console.log("RegisterUser Lambda response:", response);
 
-//         expect(response.statusCode).toBe(201);
-//         expect(response.body.ok).toBe(true);
-//         expect(response.body.userId).toBeDefined();
-//         expect(response.body.message).toBe("user successfully created");
-//         expect(response.body.accessToken).toBeDefined();
+        expect(response.statusCode).toBe(201);
+        expect(response.body.ok).toBe(true);
+        expect(response.body.userId).toBeDefined();
+        expect(response.body.message).toBe("user successfully created");
+        expect(response.body.accessToken).toBeDefined();
 
-//         accessToken = response.body.accessToken;
-//         refreshToken = response.headers["Set-Cookie"];
+        accessToken = response.body.accessToken;
+        refreshToken = response.headers["Set-Cookie"];
 
-//         console.log("RegisterUser Lambda response body:", response.body.accessToken);
+        console.log("RegisterUser Lambda response body:", response.body.accessToken);
 
-//         userId = response.body.userId;
-//         console.log("Created userId:", userId);
+        userId = response.body.userId;
+        console.log("Created userId:", userId);
 
-//         await new Promise(resolve => setTimeout(resolve, 3000));
-//         const profileAfterCreation = await getUserProfileByUserId(userId);
-//         expect(profileAfterCreation).toBeDefined();
-//         expect(profileAfterCreation.email).toBe(TEST_EMAIL);
-//         expect(profileAfterCreation.firstName).toBe(TEST_FIRSTNAME);
-//         expect(profileAfterCreation.lastName).toBe(TEST_LASTNAME);
-//     });
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        const profileAfterCreation = await getUserProfileByUserId(userId);
+        expect(profileAfterCreation).toBeDefined();
+        expect(profileAfterCreation.email).toBe(TEST_EMAIL);
+        expect(profileAfterCreation.firstName).toBe(TEST_FIRSTNAME);
+        expect(profileAfterCreation.lastName).toBe(TEST_LASTNAME);
+    });
 
-//     it("devrait permettre de créer la base d'une association", async () => {
+    it("devrait permettre de créer la base d'une association", async () => {
 
-//         const body = {
-//             name: `Association de test ${timestamp}`,
-//             description: "Cette association est un test pour la lambda de création de fiche d'associaction.",
-//             type: "association",
-//             postalCode: "94450"
-//         }
+        const body = {
+            name: `Association de test ${timestamp}`,
+            description: "Cette association est un test pour la lambda de création de fiche d'associaction.",
+            type: "association",
+            postalCode: "94450"
+        }
 
-//         let headers = {
-//             Authorization: `Bearer ${accessToken}`,
-//             Cookie: refreshToken,
-//         }
+        let headers = {
+            Authorization: `Bearer ${accessToken}`,
+            Cookie: refreshToken,
+        }
 
-//         const payload = await client.send(
-//             new InvokeCommand({
-//                 FunctionName: `site-asso-api-${process.env.STAGE}-addAsso`,
-//                 Payload: Buffer.from(JSON.stringify({
-//                     headers: headers,
-//                     body: JSON.stringify(body),
-//                 })),
-//             })
-//         );
+        const payload = await client.send(
+            new InvokeCommand({
+                FunctionName: `site-asso-api-${process.env.STAGE}-addAsso`,
+                Payload: Buffer.from(JSON.stringify({
+                    headers: headers,
+                    body: JSON.stringify(body),
+                })),
+            })
+        );
 
-//         const response = decode(payload.Payload);
+        const response = decode(payload.Payload);
 
-//         console.log("addAsso Lambda response:", response);
-//         console.log("Logged in accessToken:", response.body.accessToken);
-//         assoId = response.body.data.assoId;
+        console.log("addAsso Lambda response:", response);
+        console.log("Logged in accessToken:", response.body.accessToken);
+        assoId = response.body.data.assoId;
 
-//         expect(response).toBeDefined();
-//         expect(response.statusCode).toBe(201);
-//         expect(response.body.ok).toBe(true);
-//         expect(assoId).toBeDefined();
-//     });
+        expect(response).toBeDefined();
+        expect(response.statusCode).toBe(201);
+        expect(response.body.ok).toBe(true);
+        expect(assoId).toBeDefined();
+    });
 
-//     it("devrait permettre de générer une URL de pré-signed upload pour le logo de l'association", async () => {
-//         const body = {
-//             contentType: "image/png",
-//             size: 1 * 1024 * 1024,
-//             checksum: "fd494bd1744d057d76fe4ec6ff754f50c0151f08f33dc1784381bf95ef22c8cf",
-//             fileName: "assoLogo"
-//         }
+    it("devrait permettre de générer une URL de pré-signed upload pour le logo de l'association", async () => {
+        const body = {
+            contentType: "image/png",
+            size: 1 * 1024 * 1024,
+            checksum: "fd494bd1744d057d76fe4ec6ff754f50c0151f08f33dc1784381bf95ef22c8cf",
+            fileName: "assoLogo"
+        }
 
-//         let headers = {
-//             Authorization: `Bearer ${accessToken}`,
-//             Cookie: refreshToken,
-//         }
+        let headers = {
+            Authorization: `Bearer ${accessToken}`,
+            Cookie: refreshToken,
+        }
 
-//         const payload = await client.send(
-//             new InvokeCommand({
-//                 FunctionName: `site-asso-api-${process.env.STAGE}-generateUploadUrl`,
-//                 Payload: Buffer.from(JSON.stringify({
-//                     queryStringParameters: { assoId: assoId },
-//                     headers: headers,
-//                     body: JSON.stringify(body),
-//                 })),
-//             })
-//         );
+        const payload = await client.send(
+            new InvokeCommand({
+                FunctionName: `site-asso-api-${process.env.STAGE}-generateUploadUrl`,
+                Payload: Buffer.from(JSON.stringify({
+                    queryStringParameters: { assoId: assoId },
+                    headers: headers,
+                    body: JSON.stringify(body),
+                })),
+            })
+        );
 
-//         const response = decode(payload.Payload);
+        const response = decode(payload.Payload);
 
-//         console.log("generateUploadUrl Lambda response:", response);
-//         console.log("Logged in accessToken:", response.body.accessToken);
-//         const uploadUrl = response.body.data.uploadUrl;
+        console.log("generateUploadUrl Lambda response:", response);
+        console.log("Logged in accessToken:", response.body.accessToken);
+        const uploadUrl = response.body.data.uploadUrl;
 
-//         expect(response).toBeDefined();
-//         expect(response.statusCode).toBe(200);
-//         expect(response.body.ok).toBe(true);
-//         expect(uploadUrl).toBeDefined();
-//     });
+        expect(response).toBeDefined();
+        expect(response.statusCode).toBe(200);
+        expect(response.body.ok).toBe(true);
+        expect(uploadUrl).toBeDefined();
+    });
 
-//     it("devrait permettre de supprimer le user", { timeout: 10000 }, async () => {
+    it("devrait permettre de supprimer le user", { timeout: 10000 }, async () => {
 
-//         const body = {
-//             password: TEST_PASSWORD,
-//         }
+        const body = {
+            password: TEST_PASSWORD,
+        }
 
-//         const headers = {
-//             Authorization: `Bearer ${accessToken}`,
-//             Cookie: refreshToken,
-//         }
+        const headers = {
+            Authorization: `Bearer ${accessToken}`,
+            Cookie: refreshToken,
+        }
 
-//         const payload = await client.send(
-//             new InvokeCommand({
-//                 FunctionName: `site-asso-api-${process.env.STAGE}-removeAuthUser`,
-//                 Payload: Buffer.from(JSON.stringify({
-//                     headers: headers,
-//                     body: JSON.stringify(body),
-//                 })),
-//             })
-//         );
+        const payload = await client.send(
+            new InvokeCommand({
+                FunctionName: `site-asso-api-${process.env.STAGE}-removeAuthUser`,
+                Payload: Buffer.from(JSON.stringify({
+                    headers: headers,
+                    body: JSON.stringify(body),
+                })),
+            })
+        );
 
-//         const response = decode(payload.Payload);
+        const response = decode(payload.Payload);
 
-//         console.log("RemoveAuthUser Lambda response:", response);
+        console.log("RemoveAuthUser Lambda response:", response);
 
-//         expect(response.statusCode).toBe(200);
-//         expect(response.body.ok).toBe(true);
-//         expect(response.body.message).toBe("User removed");
+        expect(response.statusCode).toBe(200);
+        expect(response.body.ok).toBe(true);
+        expect(response.body.message).toBe("User removed");
 
-//         await new Promise(resolve => setTimeout(resolve, 3000));
-//         const profileAfterDelete = await getUserProfileByUserId(userId);
-//         console.log("profileAfterDelete", profileAfterDelete);
-//         expect(profileAfterDelete).toBeUndefined();
-//     });
-// })
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        const profileAfterDelete = await getUserProfileByUserId(userId);
+        console.log("profileAfterDelete", profileAfterDelete);
+        expect(profileAfterDelete).toBeUndefined();
+    });
+})
--- backend/tests/integration/registerUserLambda.int.test.mjs
+++ backend/tests/integration/registerUserLambda.int.test.mjs
@@ -1,121 +1,121 @@
-// import { describe, it, beforeAll, expect } from "vitest";
-// import { getUserProfileByUserId } from "../testDbRequest/get.js";
-// import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
-// import { decode } from "../../helpers/toolbox.js";
+import { describe, it, beforeAll, expect } from "vitest";
+import { getUserProfileByUserId } from "../testDbRequest/get.js";
+import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
+import { decode } from "../../helpers/toolbox.js";
 
-// const client = new LambdaClient({ region: "eu-west-3" });
+const client = new LambdaClient({ region: "eu-west-3" });
 
-// const timestamp = Date.now();
-// const EMAIL = `cyril.bonneau${timestamp}@outlook.com`;
-// const PASSWORD = "Ax9!qL7#vZ3@pT2";
-// const FIRSTNAME = "Jack";
-// const LASTNAME = "Larnaque";
-// const BAD_FIRSTNAME = "J4CK";
-// const BAD_LASTNAME = "L45N4QUE";
+const timestamp = Date.now();
+const EMAIL = `cyril.bonneau${timestamp}@outlook.com`;
+const PASSWORD = "Ax9!qL7#vZ3@pT2";
+const FIRSTNAME = "Jack";
+const LASTNAME = "Larnaque";
+const BAD_FIRSTNAME = "J4CK";
+const BAD_LASTNAME = "L45N4QUE";
 
-// describe("test registerUserLambda", () => {
-//     it("should register a new user and update the email", async () => {
-//         // Register a new user
-//         const payload = {
-//             email: EMAIL,
-//             password: PASSWORD,
-//             firstName: FIRSTNAME,
-//             lastName: LASTNAME,
-//         };
+describe.skip("test registerUserLambda", () => {
+    it("should register a new user and update the email", async () => {
+        // Register a new user
+        const payload = {
+            email: EMAIL,
+            password: PASSWORD,
+            firstName: FIRSTNAME,
+            lastName: LASTNAME,
+        };
 
-//         const request = await client.send(
-//             new InvokeCommand({
-//                 FunctionName: `site-asso-api-${process.env.STAGE}-registerUser`,
-//                 Payload: Buffer.from(JSON.stringify({ body: JSON.stringify(payload) })),
-//             })
-//         );
+        const request = await client.send(
+            new InvokeCommand({
+                FunctionName: `site-asso-api-${process.env.STAGE}-registerUser`,
+                Payload: Buffer.from(JSON.stringify({ body: JSON.stringify(payload) })),
+            })
+        );
 
-//         const response = decode(request.Payload);
+        const response = decode(request.Payload);
 
-//         console.log('Register User Lambda response:', response);
+        console.log('Register User Lambda response:', response);
 
-//         expect(response).toBeDefined();
-//         expect(response.statusCode).toBe(201);
-//         expect(response.body.userId).toBeDefined();
-//         expect(response.body.message).toBe("user successfully created");
+        expect(response).toBeDefined();
+        expect(response.statusCode).toBe(201);
+        expect(response.body.userId).toBeDefined();
+        expect(response.body.message).toBe("user successfully created");
 
-//         const userId = response.body.userId;
-//         console.log("Created userId:", userId);
+        const userId = response.body.userId;
+        console.log("Created userId:", userId);
 
-//         await new Promise(resolve => setTimeout(resolve, 3000));
-//         const profileAfterCreation = await getUserProfileByUserId(userId);
-//         expect(profileAfterCreation).toBeDefined();
-//         expect(profileAfterCreation.email).toBe(EMAIL);
-//         expect(profileAfterCreation.firstName).toBe(FIRSTNAME);
-//         expect(profileAfterCreation.lastName).toBe(LASTNAME);
-//     })
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        const profileAfterCreation = await getUserProfileByUserId(userId);
+        expect(profileAfterCreation).toBeDefined();
+        expect(profileAfterCreation.email).toBe(EMAIL);
+        expect(profileAfterCreation.firstName).toBe(FIRSTNAME);
+        expect(profileAfterCreation.lastName).toBe(LASTNAME);
+    })
 
-//     it("should fail to register a user with invalid firstName and/or lastName", async () => {
-//         const payload = {
-//             email: `invalid${Date.now()}@example.com`,
-//             password: "ValidPass123!",
-//             firstName: BAD_FIRSTNAME,
-//             lastName: BAD_LASTNAME,
-//         };
+    it("should fail to register a user with invalid firstName and/or lastName", async () => {
+        const payload = {
+            email: `invalid${Date.now()}@example.com`,
+            password: "ValidPass123!",
+            firstName: BAD_FIRSTNAME,
+            lastName: BAD_LASTNAME,
+        };
 
-//         const request = await client.send(
-//             new InvokeCommand({
-//                 FunctionName: `site-asso-api-${process.env.STAGE}-registerUser`,
-//                 Payload: Buffer.from(JSON.stringify({ body: JSON.stringify(payload) })),
-//             })
-//         );
+        const request = await client.send(
+            new InvokeCommand({
+                FunctionName: `site-asso-api-${process.env.STAGE}-registerUser`,
+                Payload: Buffer.from(JSON.stringify({ body: JSON.stringify(payload) })),
+            })
+        );
 
-//         const response = decode(request.Payload);
+        const response = decode(request.Payload);
 
-//         console.log('Register User Lambda response with invalid names:', response);
+        console.log('Register User Lambda response with invalid names:', response);
 
-//         expect(response).toBeDefined();
-//         expect(response.statusCode).toBe(400);
-//         expect(response.body.errors).toBeDefined();
-//         expect(response.body.errors.length).toBeGreaterThan(0);
-//         expect(response.body.errors.some(error => error.includes("Nom invalide"))).toBe(true);
-//     })
+        expect(response).toBeDefined();
+        expect(response.statusCode).toBe(400);
+        expect(response.body.errors).toBeDefined();
+        expect(response.body.errors.length).toBeGreaterThan(0);
+        expect(response.body.errors.some(error => error.includes("Nom invalide"))).toBe(true);
+    })
 
-//     afterAll(async () => {
-//         // Clean up: delete the user from the database
+    afterAll(async () => {
+        // Clean up: delete the user from the database
 
-//         const payload = {
-//             email: EMAIL,
-//             password: PASSWORD,
-//         }
+        const payload = {
+            email: EMAIL,
+            password: PASSWORD,
+        }
 
-//         const request = await client.send(
-//             new InvokeCommand({
-//                 FunctionName: `site-asso-api-${process.env.STAGE}-loginUser`,
-//                 Payload: Buffer.from(JSON.stringify({ body: JSON.stringify(payload) })),
-//             })
-//         );
+        const request = await client.send(
+            new InvokeCommand({
+                FunctionName: `site-asso-api-${process.env.STAGE}-loginUser`,
+                Payload: Buffer.from(JSON.stringify({ body: JSON.stringify(payload) })),
+            })
+        );
 
-//         const response = decode(request.Payload);
+        const response = decode(request.Payload);
 
-//         let accessToken = response.body.accessToken;
+        let accessToken = response.body.accessToken;
 
-//         if (userId) {
-//             const deleteRequest = await client.send(
-//                 new InvokeCommand({
-//                     FunctionName: `site-asso-api-${process.env.STAGE}-deleteUser`,
-//                     Headers: {
-//                         Authorization: `Bearer ${accessToken}`,
-//                     },
-//                     Payload: Buffer.from(JSON.stringify({ body: JSON.stringify({ userId }) })),
-//                 })
-//             );
+        if (userId) {
+            const deleteRequest = await client.send(
+                new InvokeCommand({
+                    FunctionName: `site-asso-api-${process.env.STAGE}-deleteUser`,
+                    Headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                    Payload: Buffer.from(JSON.stringify({ body: JSON.stringify({ userId }) })),
+                })
+            );
 
-//             const deleteResponse = decode(deleteRequest.Payload);
-//             console.log("Delete User Lambda response:", deleteResponse);
-//             expect(deleteResponse.body.ok).toBe(true);
-//             expect(deleteResponse.statusCode).toBe(200);
-//             expect(deleteResponse.body.message).toBe("User removed");
+            const deleteResponse = decode(deleteRequest.Payload);
+            console.log("Delete User Lambda response:", deleteResponse);
+            expect(deleteResponse.body.ok).toBe(true);
+            expect(deleteResponse.statusCode).toBe(200);
+            expect(deleteResponse.body.message).toBe("User removed");
 
-//             await new Promise(resolve => setTimeout(resolve, 3000));
-//             const profileAfterDelete = await getUserProfileByUserId(userId);
-//             console.log("profileAfterDelete", profileAfterDelete);
-//             expect(profileAfterDelete).toBeUndefined();
-//         }
-//     });
-// });
+            await new Promise(resolve => setTimeout(resolve, 3000));
+            const profileAfterDelete = await getUserProfileByUserId(userId);
+            console.log("profileAfterDelete", profileAfterDelete);
+            expect(profileAfterDelete).toBeUndefined();
+        }
+    });
+});
